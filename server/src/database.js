65b0c092d835c00ff3b1287a5661273580511b93
Amarnath AS <amarnathas2003@gmail.com>
2024-04-13 18:26:40 +0530

    fix: production build

---
+++ server/src/database.js
@@ -0,0 +1,31 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const app_1 = __importDefault(require("./app"));
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect(process.env.CONNECTION_STRING);
+            console.log("MongoDB Connected");
+            app_1.default.listen(process.env.PORT, () => {
+                console.log(`App is listening to ${process.env.PORT}`);
+            });
+        }
+        catch (error) {
+            console.error("Connection Error", error);
+        }
+    });
+}
+connect();
