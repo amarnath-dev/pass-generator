b2801e9f364477b410c612af8eb1559636484923
Amarnath AS <amarnathas2003@gmail.com>
2024-04-18 18:07:03 +0530

    fix: input field added missed validation

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,8 +6,8 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 const corsConfig = {
-  origin: "http://localhost:5173",
-  // origin: "https://pass-generator-z55c.onrender.com",
+  // origin: "http://localhost:5173",
+  origin: "https://pass-generator-z55c.onrender.com",
   credentials: true,
 };
 
